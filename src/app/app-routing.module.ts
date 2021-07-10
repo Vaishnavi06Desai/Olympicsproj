f6c7d89d227bca4349d9f9b8f5dc5eac569cb7c1
unknown <jhanvisdesai@gmail.com>
2021-07-11 02:04:38 +0530

    get changes at 2 am

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { TorchComponent } from './torch/torch.component';
 import { PuzzleComponent } from './puzzle/puzzle.component';
 import { NonogramComponent } from './nonogram/nonogram.component';
 import { BoardComponent } from './board/board.component';
+import { BallsComponent } from './balls/balls.component';
 const routes: Routes = [
   {
     path: "signin",
@@ -29,6 +30,10 @@ const routes: Routes = [
   {
     path:"torch",
     component: TorchComponent
+  },
+  {
+    path:"balls",
+    component: BallsComponent
   },
   {
     path:"board",
