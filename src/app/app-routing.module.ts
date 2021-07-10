1244765dbd21ba140640b34c573f8fef8b6bf235
anika-001 <dream.sweeter.30.09@gmail.com>
2021-07-10 15:06:03 +0530

    password reset

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SigninComponent } from './signin/signin.component';
-
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 const routes: Routes = [
   {
     path: "signin",
     component: SigninComponent
+  }
+  ,{
+    path: "forgotpassword",
+    component: ForgotPasswordComponent
   }
 ];
 
